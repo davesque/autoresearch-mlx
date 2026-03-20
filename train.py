21559f8475ca67e1ca7dd70b9b33be65b0530530
David Sanders <davesque@gmail.com>
2026-03-19 18:19:35 -0700

    experiment: VE on all layers (instead of alternating)

--- train.py
+++ train.py
@@ -35,8 +35,8 @@ def norm(x):
 
 
 def has_ve(layer_idx, n_layer):
-    """Returns True if layer should have Value Embedding (alternating, last always included)."""
-    return layer_idx % 2 == (n_layer - 1) % 2
+    """Returns True if layer should have Value Embedding (all layers)."""
+    return True
 
 
 def create_additive_causal_mask(seq_len, dtype=mx.float32):
